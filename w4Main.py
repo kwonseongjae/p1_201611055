5f5bbc045382d325a67a97bf116338fba84e2002
kwonseongjae <taiban71@naver.com>
2016-03-30 16:44:37 +0900

    권성재4주차-4과제

---
+++ w4Main.py
@@ -0,0 +1,11 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def makeSwirlsSquare(size,bigger,sides,angle):
+    for i in range(0,sides):
+        if(i%2):
+            size=size+bigger
+            t1.fd(size)
+            t1.right(angle)
+makeSwirlsSquare(10,10,30,90)
+wn.exitonclick()
