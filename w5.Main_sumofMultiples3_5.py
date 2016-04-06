9cdff0dfd7aa5b4f6cdc0ca75ab8c57127ec3606
kwonseongjae <taiban71@naver.com>
2016-04-06 15:29:01 +0900

    6주차_1000이하의3,5배수의합
    6주차_1000이하의3,5배수의합

---
+++ w5.Main_sumofMultiples3_5.py
@@ -0,0 +1,13 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def sumofMultiplesof3_5(begin,end):
+    sum=0
+    for i in range(begin,end):
+        if not i%3 or i%5:
+                sum=sum+i
+    print (sum)
+sumofMultiplesof3_5(0,1000)
+
+wn.exitonclick()
