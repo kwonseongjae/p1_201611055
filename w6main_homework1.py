ab0d3389a80f9fafa25216f33b5285ef03ea91f3
kwonseongjae <taiban71@naver.com>
2016-04-07 12:27:25 +0900

    6주차_윤년맞추기
    6주차_윤년맞추기

---
+++ w6main_homework1.py
@@ -0,0 +1,17 @@
+﻿import turtle 
+wn=turtle.Screen() 
+t1=turtle.Turtle() 
+
+def lab6(): 
+    year=input("input year{2000~}: ") 
+    y1=int(year) 
+    if(y1%4==0): 
+        print ("This year is leap year") 
+    else: 
+        print ("This year is not leap year") 
+
+def main(): 
+    lab6() 
+
+main() 
+wn.exitonclick()
