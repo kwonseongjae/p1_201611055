60ac8314382125541a6d29400c0e9121d3749a4c
kwonseongjae <taiban71@naver.com>
2016-05-04 00:23:26 +0900

    w8main과제완료
    w8main과제완료

---
+++ w8main1/w8main1-1.py
@@ -0,0 +1,15 @@
+﻿sel=input("Enter the words: ")
+k=dict() 
+word=sel 
+for c in word: 
+    if c not in k: 
+        k[c]=1 
+    else: 
+        k[c]=k[c]+1 
+print (k)
+import matplotlib 
+import matplotlib.pyplot as plt 
+plt.bar(range(len(k)),k.values(),align='center') 
+plt.xticks(range(len(k)),list(k.keys())) 
+plt.show()
+wn.exitonclick()
---
+++ w8main1/w8main1-2.py
@@ -0,0 +1,17 @@
+﻿import turtle
+wn=turtle.Screen()
+word = '7 Hongji'   
+k=dict()  
+k['str']=0  
+k['int']=0  
+for i in word:  
+    if i.isalpha():  
+        k['str']=k['str']+1  
+    elif i.isdigit:  
+        k['int']=k['int']+1  
+print (k) 
+
+import matplotlib  
+import matplotlib.pyplot as plt 
+
+wn.exitonclick()
