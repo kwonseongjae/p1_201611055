ed8127d2b9eb505753e553fad42d393c491d0054
kwonseongjae <taiban71@naver.com>
2016-05-17 22:33:32 +0900

    11주차과제완료
    11주차과제완료

---
+++ w11main5.py
@@ -0,0 +1,35 @@
+﻿import turtle  
+wn=turtle.Screen()  
+t1=turtle.Turtle()  
+import math  
+t1.pu()  
+t1.goto(0,50)  
+t1.pd()  
+t1.circle(50)  
+t1.penup()  
+t1.home()  
+def isIncircle(curpos):  
+    radius=50  
+    pos=(0,100)  
+    if math.sqrt(math.pow(curpos[0]-pos[0],2) + math.pow(curpos[1]-pos[1],2))<=radius:  
+        print ("True")  
+    else:  
+        print ("False")  
+def k1():  
+    t1.fd(30)  
+    curpos=t1.pos()  
+    isIncircle(curpos)  
+wn.onkey(k1,"Up")  
+def k2():  
+    t1.lt(90)  
+def k3():  
+    t1.right(90)  
+def k4():  
+    t1.bk(30)  
+    curpos=t1.pos()  
+    isIncircle(curpos)  
+wn.onkey(k2,"Left")  
+wn.onkey(k3,"Right")  
+wn.onkey(k4,"Down")  
+wn.listen()  
+wn.exitonclick() 
