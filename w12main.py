c25128411ddf3e565f9f0a4332409389020f74bc
kwonseongjae <taiban71@naver.com>
2016-05-24 13:17:15 +0900

    12주차과제완료
    12주차과제완료

---
+++ w12main.py
@@ -0,0 +1,53 @@
+﻿import os
+mydir = os.getcwd()
+
+
+def homework1():
+    mydir=os.getcwd()
+    filename='python.txt'
+    myfilename=os.path.join(mydir, filename)
+
+    try :
+        myfile=open(myfilename, 'r')
+    except IOError as e:
+        print(e)
+    contents=myfile.readlines()
+    myfile.close()
+    for content in contents :
+        if content.lower().find('python')!=-1 :
+            print(content)
+
+def homework2():
+    myfile=open('output.txt', 'w')
+    line1='first line\n'
+    myfile.write(line1)
+    line2='\tsecond line\n'
+    myfile.write(line2)
+    line3='third line'
+    myfile.write(line3)
+    myfile.close()
+
+    mydir=os.getcwd()
+    filename='output.txt'
+    myfilename=os.path.join(mydir,filename)
+    print (myfilename)
+
+    try:
+        myfile=open(myfilename,'r')
+
+    except IOError as e :
+            print ('error')
+
+    contents=myfile.readlines()
+    myfile.close()
+
+    for i in contents:
+        if (-1 < i.find("line")):
+            print(i.upper())
+
+def main():
+    homework1()
+    homework2()
+
+
+main()
