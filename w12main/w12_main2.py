379b25cdfa098c55027388d291a3c36f518b7fdf
kwonseongjae <taiban71@naver.com>
2016-05-27 01:19:29 +0900

    12주차과제완료
    12주차과재완료

---
+++ w12main/w12_main2.py
@@ -0,0 +1,54 @@
+﻿import os
+mydir = os.getcwd()
+
+
+def lab12_1() :
+    mydir=os.getcwd()
+    filename='python.txt'
+    myfilename=os.path.join(mydir, filename)
+    try :
+        myfile=open(myfilename, 'r')
+    except IOError as e:
+        print (e)
+    contents=myfile.readlines()
+    myfile.close()
+    for content in contents :
+        if content.lower().find('python')!=-1 :
+            print(content)
+
+def lab12_2():
+ 
+    myfile=open('output.txt', 'w')
+    line1='first line\n'	
+    myfile.write(line1)
+    line2='\tsecond line\n'
+    myfile.write(line2)
+    line3='third'
+    myfile.write(line3)
+    myfile.close()
+
+    mydir=os.getcwd()
+    filename='output.txt'
+    myfilename=os.path.join(mydir,filename)
+    print (myfilename)
+
+
+    try:
+        myfile=open(myfilename,'r')
+
+    except IOError as e :
+        print ('erorr')
+
+    contents=myfile.readlines()
+    myfile.close()
+
+    for i in contents:
+        if (-1 < i.find("line")):
+            print (i.upper())
+
+def main():
+    lab12_1()
+    lab12_2()
+
+
+main()
---
+++ w12main/w12_main3.py
@@ -0,0 +1,36 @@
+﻿import time
+def lab12_1():
+    fin=open('output.txt', 'r')
+    fout=open('outputUpper.txt','w')
+    add='[khb edited {0}]'.format(time.strftime('%Y.%m.%d , %H:%M:%S'))
+    for line in fin:
+        words=line.split()
+        fout.write(add)
+        fout.write('\t')
+        for word in words:
+            if word == 'line':
+                fout.write('\t')    
+                word=word.upper()
+            fout.write(word)
+        fout.write('\n')
+    fin.close()
+    fout.close()
+def lab12_2():
+    data=[1,2,3,4,5,6,7,8,9,10]
+    fout=open('outputHB.txt','w')
+    for i in data:
+        toPrint="{0}\t".format(i)
+    #print toPrint,
+    fout.write(toPrint)
+    if i%2==0:
+            fout.write('\n')
+    fout.close()
+
+def main():
+    lab12_1()
+    lab12_2()
+
+if __name__=="__main__":
+    main()
+
+sel1=input()
