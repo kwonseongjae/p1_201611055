e2aaea0784e6d4e2e4c13a4b2f7c32f1cb602523
kwonseongjae <taiban71@naver.com>
2016-05-30 21:22:58 +0900

    12주차과제완료
    12주차과제완료

---
+++ w12main2/w12_main4.py
@@ -0,0 +1,25 @@
+﻿aFile=input("Input filename ex)recoords.txt: ")
+
+def getCoordsFromFile(aFile):
+    fileopen=open(aFile)
+    coords=list()
+    for line in fileopen:
+        line1=line.split(',')
+        coords.append([(line1[0],line1[1]), (line1[2],line1[3].strip())])
+    fileopen.close()
+    return coords
+coords=getCoordsFromFile(aFile)
+
+def drawSquareWithCoords(coords):
+    for coord in coords:
+        x1=int(coord[0][0])
+        x2=int(coord[1][0])
+        y1=int(coord[0][1])
+        y2=int(coord[1][1])
+    print (x1,y1,x2,y2)
+    t1.goto(x1,y1)
+    for i in range(0,4):
+        t1.fd(x1-x2)
+        t1.rt(90)
+drawSquareWithCoords(coords)
+wn.exitonclick()
---
+++ w12main2/w12_main5.py
@@ -0,0 +1,10 @@
+﻿try:
+    fin1=open('python.txt','a')
+    fin2=open('outputNum.txt','r')
+    for line in fin2:
+        fin1.write(line)
+    fin1.close()
+    fin2.close()
+except Exception as e:
+    print(e)
+wn.exitonclick()
