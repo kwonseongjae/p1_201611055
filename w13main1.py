6f8d684044c973551363f2b48d0a2df70f808bfb
kwonseongjae <taiban71@naver.com>
2016-06-01 18:18:49 +0900

    13주차 동물울음소리
    13주차 동물울음소리

---
+++ w13main1.py
@@ -0,0 +1,20 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print ("my dog name is",self.name)
+    def talk(Dog):
+        print ("mung mung")
+        
+class ShihTzu(object):
+    def talk(Dog):
+        print ("si si")
+
+class Maltese(object):
+    def talk(Dog):
+        print ("mal mal")
+mydog=Maltese()
+mydog.talk()
+import turtle
+wn=turtle.Screen()
+wn.exitonclick()
